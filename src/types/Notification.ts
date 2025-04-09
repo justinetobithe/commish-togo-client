5f7b49d9bc6eec270589256d1a62b017a0c52fc8
Justine Tobithe Doloiras <justine.tobithe27@gmail.com>
2025-04-10 05:04:17 +0800

    Full updates

--- src/types/Notification.ts
+++ src/types/Notification.ts
@@ -1,14 +1,14 @@
 export default interface Notification {
-    id: string
-    type: string
-    notifiable_type: string
-    notifiable_id: number
+    id: string;
+    type: string;
+    notifiable_type: string;
+    notifiable_id: number;
     data: {
-        user: string
-        message: string
-        created_at: string
-    }
-    read_at: any
-    created_at: string
-    updated_at: string
+        user: string;
+        message: string;
+        created_at: string;
+    };
+    read_at: string | null; 
+    created_at: string;
+    updated_at: string;
 }
